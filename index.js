fbf561d4612644149cc3282f5ef56f289aae89d9
Gabriel Moreira <gabss.moreira@icloud.com>
2022-02-19 12:29:01 -0300

    retornar da secao 1.2, falando de tipos

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+// variavel constante
+const nome='Gabriel'
+const idade=21
+
+
+// let é basicamente igual ao var, porém mais moderno
+// let é local 
+// var é global
+let a=2
+a=3
+a="abc"
+
+var c = 2+3
+*/
+// rodar no terminal: node nome_arquivo.js
+var linguagem = 'JavaScript'
+console.log('Aprendendo '+ linguagem)
+
+var linguagem = 'Java'
+console.log('Aprendendo '+ linguagem)
+console.log('\n')
+
+var idade = 21
+console.log('Oi, ' + nome)
+if (idade >=18){
+    var nome = 'Gabriel'
+    console.log("parabens, " + nome)
+    console.log('voce pode dirigir')
+}
+console.log('ate mais ' + nome)
+console.log('\n')
