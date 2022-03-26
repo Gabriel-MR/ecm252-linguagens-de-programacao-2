fbc009fe434accb327c8f381caf71efac2918b1a
Gabriel Moreira <gabss.moreira@icloud.com>
2022-03-26 12:48:59 -0300

    feat(mss_informacoes): requisao api

---
+++ Aulas/aula5_mss_lembrete s_informacoes/mss_informacao/index.js
@@ -0,0 +1,22 @@
+/**
+ npm run SCRIPT (apontado para o package.json)
+ */
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+//POST
+//host:porta/lembretes/123456/observacoes
+app.post('/lembretes/:id/observacoes', (req, res) => {
+
+})
+
+//GET
+app.get('/lembretes/:id/observacoes', (req, res) => {
+
+})
+
+app.listen(5000, () => {
+    console.log('Lembretes. Porta 5000.')
+})
