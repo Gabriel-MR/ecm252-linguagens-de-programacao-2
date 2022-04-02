9a6410bee34442b45af1dc7c6c5dee98739e48fe
Gabriel Moreira <gabss.moreira@icloud.com>
2022-04-02 11:08:19 -0300

    feat(mss_informacao): rodando

--- Aulas/aula5_mss_lembrete s_informacoes/mss_informacao/index.js
+++ Aulas/aula5_mss_lembrete s_informacoes/mss_informacao/index.js
@@ -2,21 +2,31 @@
  npm run SCRIPT (apontado para o package.json)
  */
 
-const express = require('express')
-const app = express()
-app.use(express.json())
+const express = require('express');
+const {v4: uuidv4} = require('uuid')
+const app = express();
+app.use(express.json());
 
-//POST
+const observacoesPorLembreteId = {
+
+}
+
+//POST - end point
 //host:porta/lembretes/123456/observacoes
 app.post('/lembretes/:id/observacoes', (req, res) => {
-
+    const idOb = uuidv4()
+    // const texto = req.body.texto //só podemos usar o req.body porque declaramos na linha 8
+    const {texto} = req.body
+    const observacoesDoLembrete = observacoesPorLembreteId[req.params.id] || [] // poderia ser feito com um if
+    observacoesDoLembrete.push({id: idOb, texto: texto})
+    observacoesPorLembreteId[req.params.id] = observacoesDoLembrete
 })
 
-//GET
+//GET - request
 app.get('/lembretes/:id/observacoes', (req, res) => {
 
 })
 
-app.listen(5000, () => {
-    console.log('Lembretes. Porta 5000.')
+app.listen(4000, () => {
+    console.log('Lembretes. Porta 4000.')
 })
